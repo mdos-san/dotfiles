f56b618fd01ef198b294c53a9f32ef2d5ded6fde
Mickael Dos Santos <mickael.dos-santos@polyconseil.fr>
2022-10-24 20:10:30 +0200

    neovim: Link JSX, TS & TSX to JS snippet

--- neovim/.config/nvim/lua/snippets/init.lua
+++ neovim/.config/nvim/lua/snippets/init.lua
@@ -1,9 +1,2 @@
-local ls = require("luasnip")
-
-ls.add_snippets("all", {
-    require("./snippets/typescript/ser")
-})
-
--- load snippets from path/of/your/nvim/config/my-cool-snippets
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
 
